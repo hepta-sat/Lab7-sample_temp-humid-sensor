31245fbe75faf9ba00eceb0e8801918a9b19a64b
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-03-04 15:24:17 +0000

    temp&humid_sample

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
+#include "Dht11.h"
+ 
+Serial pc(USBTX, USBRX);
+Dht11 sensor(p15);
+int temp,humid;
 
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+     while(1){
+        sensor.read();
+        temp = sensor.getFahrenheit();
+        humid = sensor.getHumidity();
+        temp = (temp - 32.0)/1.80;    // Fahrenheit --> Celsius
+        pc.printf("T: %d, H: %d\r\n", temp, humid);
+        wait(1.0);
+    }
 }
